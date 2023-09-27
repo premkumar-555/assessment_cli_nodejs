0c6072ecda7a92544333de670a38f286a449b90b
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-09-27 16:07:08 +0530

    created cli options using commander

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+const {Command} = require('commander')
+const figlet = require('figlet')
+const path = require("path")
+
+console.log(figlet.textSync("Dir Manager"));
+// Initiating CLI program with options
+const program = new Command();
+
+program
+.version("1.0.0")
+.description("A CLI to read data files and return back the results in a tabular form")
+.option("-epc, --epc <value>", "command to print which endpoint is called how many times")
+.option("-pmc, --pmc <value>", "command to print how many API calls were being made on per minute basis")
+.option("-scc, --scc <value>", "command to print how many API calls are there in total for each HTTP status code")
+.parse(process.argv)   // for parsing command-line arguments
+
+// options
+const options = program.opts();
+
+if(options.epc){
+   console.log('epc')
+}
+if(options.pmc){
+    console.log('pmc')
+    
+}
+if(options.scc){
+    console.log('scc')
+  
+}
